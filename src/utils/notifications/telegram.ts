143b749a9ee34202ad34cec6c713f681210a32d9
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2025-01-28 20:10:49 +0100

    ci telegram env with chat

--- src/utils/notifications/telegram.ts
+++ src/utils/notifications/telegram.ts
@@ -2,7 +2,7 @@ import TelegramBot from "node-telegram-bot-api";
 
 // Ваш токен от BotFather
 const botToken = `${process.env.TELEGRAM_BOT_TOKEN}`;
-const chatId = `${process.env.TELEGRAM_CHAT_BOT_ID}`;
+const chatId = `${process.env.TELEGRAM_CHAT_ID}`;
 
 // Инициализация бота
 const bot = new TelegramBot(botToken, { polling: false });
